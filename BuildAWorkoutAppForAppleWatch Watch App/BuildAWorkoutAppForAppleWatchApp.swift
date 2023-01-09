46b3eba4d6c4b50603a2747c24aada76c66794e4
Robin <robinmurray0307@gmail.com>
2023-01-09 17:03:14 +0000

    Initial Commit

---
+++ BuildAWorkoutAppForAppleWatch Watch App/BuildAWorkoutAppForAppleWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildAWorkoutAppForAppleWatchApp.swift
+//  BuildAWorkoutAppForAppleWatch Watch App
+//
+//  Created by Robin Murray on 09/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BuildAWorkoutAppForAppleWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
